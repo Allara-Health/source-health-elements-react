8ea56eb017ca520a5d71eaab2721979eb2aa431c
Frances Hughes <francesbhughes@gmail.com>
2024-06-04 15:14:35 -0400

    feat(message): add attach file support to MessageInput

---
+++ src/components/communications/MessageInput/AttachFileButton.tsx
@@ -0,0 +1,81 @@
+import { AttachmentIcon } from '@chakra-ui/icons'
+import { IconButton, useToast } from '@chakra-ui/react'
+import React, { useCallback, useRef } from 'react'
+
+import { FileInfo } from './FilePreview'
+
+export const AttachFileButton = ({
+  setFiles,
+}: {
+  setFiles: (files: FileInfo[]) => void
+}): JSX.Element => {
+  const toast = useToast()
+  const importFileRef = useRef<HTMLInputElement>(null)
+
+  const handleChooseFile = useCallback(() => {
+    if (importFileRef.current && importFileRef.current.files?.length) {
+      const fileInfos: FileInfo[] = []
+
+      for (let i = 0; i < importFileRef.current.files.length; i++) {
+        const file = importFileRef.current.files.item(i)
+        console.log(file)
+
+        if (file) {
+          if (file.size > 5000000) {
+            toast({
+              status: 'error',
+              title: 'Upload failsed',
+              description: 'File size is too large',
+            })
+          } else {
+            fileInfos.push({
+              name: file.name,
+              size: file.size,
+              type: file.type,
+              url: URL.createObjectURL(file),
+            })
+          }
+        }
+      }
+
+      setFiles(fileInfos)
+    }
+  }, [toast])
+
+  const handleButtonClick = () => {
+    importFileRef.current?.click()
+  }
+
+  return (
+    <>
+      <input
+        multiple
+        id="choose-file-input"
+        ref={importFileRef}
+        type="file"
+        accept="image/*,.pdf"
+        onChange={handleChooseFile}
+        style={{ display: 'none' }}
+      />
+      <IconButton
+        size="XS"
+        aria-label="Attach File"
+        icon={<AttachmentIcon />}
+        onClick={handleButtonClick}
+        variant="ghost"
+        pr={2}
+        sx={{
+          _hover: {
+            background: 'none',
+          },
+          _focus: {
+            boxShadow: 'none',
+          },
+          _active: {
+            background: 'none',
+          },
+        }}
+      />
+    </>
+  )
+}
--- src/components/communications/MessageInput/MessageInputSimple.tsx
+++ src/components/communications/MessageInput/MessageInputSimple.tsx
@@ -1,35 +1,57 @@
-import React, { FunctionComponent, useCallback, useRef } from 'react'
+import { ChakraProvider, GridItem, Grid } from '@chakra-ui/react'
+import React, { FunctionComponent, useCallback, useRef, useState } from 'react'
 
 import { useMessageInputContext } from '../../../context/input'
 import { useClassFactory, useFocusTracking } from '../../../hooks'
 import { AutosizeTextarea } from '../../AutosizeTextarea'
 
+import { AttachFileButton } from './AttachFileButton'
+import { FileInfo, FilePreview } from './FilePreview'
 import { SendMessageButton } from './SendMessageButton'
 
 export const MessageInputSimple: FunctionComponent<unknown> = () => {
   const className = useClassFactory('comms', 'message-input')
   const ref = useRef<HTMLTextAreaElement>(null)
   const { text, handleChange, send } = useMessageInputContext()
+  const [files, setFiles] = useState<FileInfo[]>([])
   const isFocused = useFocusTracking(ref)
   const classNames = [
     className(),
     isFocused ? className('focused') : '',
-    text ? className('has-text') : '',
+    text || files.length > 0 ? className('has-text') : '',
   ].join(' ')
   const handleClick = useCallback(() => send(), [send])
 
+  const handleRemoveFile = (index: number) => {
+    setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index))
+  }
+
   return (
-    <div className={classNames}>
-      <AutosizeTextarea
-        className={className('textarea')}
-        placeholder="Enter message..."
-        ref={ref}
-        onChange={handleChange}
-        value={text}
-      />
-      <div className={className('action-buttons')}>
-        <SendMessageButton className={className('send-button')} size={20} onClick={handleClick} />
+    <ChakraProvider>
+      <div className={classNames}>
+        <div className={className('input-area')}>
+          <AutosizeTextarea
+            className={className('textarea')}
+            placeholder="Enter message..."
+            ref={ref}
+            onChange={handleChange}
+            value={text}
+          />
+          <div className={className('action-buttons')}>
+            <AttachFileButton setFiles={setFiles} />
+            <SendMessageButton
+              className={className('send-button')}
+              size={20}
+              onClick={handleClick}
+            />
+          </div>
+        </div>
+        {files.length > 0 && (
+          <div className={className('file-previews')}>
+            <FilePreview files={files} onRemove={handleRemoveFile} />
+          </div>
+        )}
       </div>
-    </div>
+    </ChakraProvider>
   )
 }
